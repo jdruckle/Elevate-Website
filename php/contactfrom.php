b4c824881c5ba16a8fe5cdd8fc3422944c57a64d
Jordan <jordanruckle@yahoo.com>
2018-06-19 19:38:29 -0600

    Updated Contact

--- php/contactfrom.php
+++ php/contactfrom.php
@@ -43,7 +43,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "elissa@elevatewyoming.com";
 
 
 // Configuration option.
