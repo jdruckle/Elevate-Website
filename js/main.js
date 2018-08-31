db0d2c40aa0f766dc7ba3cdd605107450934b2a1
Jordan <jordanruckle@yahoo.com>
2018-08-31 15:30:39 -0600

    Large update Added additional popup for contact below the tripanel

--- js/main.js
+++ js/main.js
@@ -168,3 +168,31 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #482628}";
         document.body.appendChild(css);
     };
+
+// Second modal functionality
+		$(document).ready(function() {
+		    var panels = $('.vote-results');
+		    var panelsButton = $('.dropdown-results');
+		    panels.hide();
+
+		    //Click dropdown
+		    panelsButton.click(function() {
+		        //get data-for attribute
+		        var dataFor = $(this).attr('data-for');
+		        var idFor = $(dataFor);
+
+		        //current button
+		        var currentButton = $(this);
+		        idFor.slideToggle(400, function() {
+		            //Completed slidetoggle
+		            if(idFor.is(':visible'))
+		            {
+		                currentButton.html('Hide Results');
+		            }
+		            else
+		            {
+		                currentButton.html('View Results');
+		            }
+		        })
+		    });
+		});
