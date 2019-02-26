ad49b3f494c52874eaccb3542f3b9560525297f6
Jordan <jordanruckle@yahoo.com>
2019-02-26 11:40:24 -0700

    First go at implementing Captcha

--- php/contactform.php
+++ php/contactform.php
@@ -70,8 +70,16 @@ if(get_magic_quotes_gpc()) {
 
 }
 
+// Import captcha files
+include_once $_SERVER['DOCUMENT_ROOT'] . '../securimage/securimage.php';
+$securimage = new Securimage();
 
-
+// Check captcha
+if ($securimage->check($_POST['captcha_code']) == false) {
+  echo "The security code entered was incorrect.<br /><br />";
+  echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
+  exit;
+}
 
 
 // Configuration option.
